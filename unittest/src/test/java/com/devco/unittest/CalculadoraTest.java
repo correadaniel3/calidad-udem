420da1f345eb13f1bbe4f9e80ef7487163f1b84f
Daniel Correa Arango <daniel.correa@smith.co>
2019-11-05 16:45:08 -0500

    Se corrigen pruebas

--- unittest/src/test/java/com/devco/unittest/CalculadoraTest.java
+++ unittest/src/test/java/com/devco/unittest/CalculadoraTest.java
@@ -1,5 +1,6 @@
 package com.devco.unittest;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -14,19 +15,33 @@ import static org.hamcrest.Matchers.is;
 @RunWith(JUnit4.class)
 public class CalculadoraTest
 {
-	@Spy
-	Calculadora calculadora;
 
-	@Before
-	public void setUp(){
-		 calculadora=new Calculadora();
-	}
+	@Spy
+	Calculadora calculadora=Mockito.spy(new Calculadora());
+
 	@Test
 	public void cuandoIngresa5EntoncesRetornaV(){
-		Mockito.when(calculadora.construirRomano(Mockito.any())).thenReturn("V");
+		Mockito.when(calculadora.construirRomano(Mockito.any(Integer.class))).thenReturn("V");
 		String respuesta = calculadora.calcular(5);
+		Assert.assertEquals(respuesta,"V");
+	}
 
-		assertThat(respuesta, is(equalTo("V")));
+	@Test
+	public void cuandoIngresaNumeroDistintoA5EntoncesRetornaI(){
+		Mockito.when(calculadora.construirRomano(Mockito.any(Integer.class))).thenReturn("I");
+		String respuesta = calculadora.calcular(6);
+		Assert.assertEquals(respuesta,"I");
+	}
 
+	@Test
+	public void cuandoIngresaNumeroDistintoA5EntoncesRetornaIConstruirRomano(){
+		String respuesta = calculadora.construirRomano(6);
+		Assert.assertEquals(respuesta,"I");
+	}
+
+	@Test
+	public void cuandoIngresa5EntoncesRetornaVConstruirRomano(){
+		String respuesta = calculadora.construirRomano(5);
+		Assert.assertEquals(respuesta,"V");
 	}
 }
